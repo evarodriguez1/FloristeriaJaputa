1037574b8cbe5d807a43480c9c8ab136f76deb68
maeva <maevarodriguez1@gmail.com>
2023-08-05 20:30:03 +0200

    add product class

---
+++ src/main/java/com/floristeria/FloristeriaJaputa/model/Product.java
@@ -0,0 +1,28 @@
+package com.floristeria.FloristeriaJaputa.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+public class Product {
+
+    @Getter @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter @Setter
+    private String name;
+    @Getter @Setter
+    private double price;
+
+    public Product() {}
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+}
