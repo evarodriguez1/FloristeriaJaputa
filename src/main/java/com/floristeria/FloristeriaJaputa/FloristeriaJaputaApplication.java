28fad1e3b9304cbe98b9bc1dae819d5d61424cbe
maeva <maevarodriguez1@gmail.com>
2023-08-05 20:05:19 +0200

    Initial commit

---
+++ src/main/java/com/floristeria/FloristeriaJaputa/FloristeriaJaputaApplication.java
@@ -0,0 +1,13 @@
+package com.floristeria.FloristeriaJaputa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloristeriaJaputaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloristeriaJaputaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/floristeria/FloristeriaJaputa/FloristeriaJaputaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floristeria.FloristeriaJaputa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloristeriaJaputaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
